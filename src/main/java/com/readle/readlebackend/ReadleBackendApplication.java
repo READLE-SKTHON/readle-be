4e5f62a610da19c44d02c8ed08e7efda213e1411
wjdah1028 <wjdah1028@naver.com>
2026-09-09 15:35:44 +0900

    :tada: Start: readle 백엔드 프로젝트 생성

---
+++ src/main/java/com/readle/readlebackend/ReadleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.readle.readlebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadleBackendApplication.class, args);
+    }
+
+}
