0ddea7ba0a22978fb82a5fde10e762f39b2d7704
wjdah1028 <wjdah1028@naver.com>
2026-09-09 16:00:37 +0900

    :wrench: Settings: exception 패키지 생성

---
+++ src/main/java/com/readle/readlebackend/global/exception/model/BaseErrorCode.java
@@ -0,0 +1,12 @@
+package com.readle.readlebackend.global.exception.model;
+
+import org.springframework.http.HttpStatus;
+
+public interface BaseErrorCode {
+
+    String getCode();
+
+    String getMessage();
+
+    HttpStatus getStatus();
+}
