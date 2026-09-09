cca29b642499123d1b3951405cd7a1b16d87609c
wjdah1028 <wjdah1028@naver.com>
2026-09-09 16:01:28 +0900

    :recycle: Refactor: EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/readle/readlebackend/ReadleBackendApplication.java
+++ src/main/java/com/readle/readlebackend/ReadleBackendApplication.java
@@ -2,8 +2,10 @@ package com.readle.readlebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ReadleBackendApplication {
 
     public static void main(String[] args) {
