43fcdc3f81fdd80e366d388b2a7e08b75aa8ffc0
wjdah1028 <wjdah1028@naver.com>
2026-09-12 00:34:13 +0900

    :twisted_rightwards_arrows: Merge: 충돌 병합

--- src/main/java/com/readle/readlebackend/domain/user/repository/UserRepository.java
+++ src/main/java/com/readle/readlebackend/domain/user/repository/UserRepository.java
@@ -4,8 +4,11 @@ import com.readle.readlebackend.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByNickname(String nickname);
 
     // xp 내림차순으로 전체 유저 조회 (전체 랭킹 조회용)
     List<User> findAllByOrderByXpDesc();
